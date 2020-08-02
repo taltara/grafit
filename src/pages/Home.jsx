e03aff52b2e1b1aac8731a0ded30e9131bc8101a
taltara <taltarablus@gmail.com>
2020-08-02 16:04:36 +0300

    Worked on site responsiveness for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import { ReactComponent as IconTv } from '../assets/img/tv.svg';
-import { ReactComponent as IconFilm } from '../assets/img/film.svg';
+import { ReactComponent as IconTv } from "../assets/img/tv.svg";
+import { ReactComponent as IconFilm } from "../assets/img/film.svg";
 
 const Home = (props) => {
+  const { theme } = props;
 
-    const { theme } = props;
-    console.log(theme);
-    return (
-        <main className={`home-main main-${theme} flex column align-end space-start`} >
-            <section className="home-content flex column align-center space-center">
+  const [headerTitleClass, setHeaderTitleClass] = useState("enter-title");
+  const [contentClass, setContentClass] = useState("");
 
-                <h1 className="home-header">GRAFIT.</h1>
-                <p>Your go-to tool for researching and analyzing your favorite Movies & TV Shows,
-                with style! </p>
-                <p>Get to know the shows you grew on even better, or get a beautiful sneak
-                    peek at your next binge-worthy journey! </p>
-                <div className="home-images flex align-center space-between">
-                    <IconTv className="type-icon" />
-                    <IconFilm className="type-icon" />
-                </div>
-                <p>Explore the bigger picture of any movie you can think of, with a rare insight you've
-                    never seen before.</p>
+  useEffect(() => {
+    setTimeout(() => {
+        setHeaderTitleClass("");
+      setTimeout(() => {
+        setHeaderTitleClass("exit-title");
+        setTimeout(() => {
+            setContentClass("new-header-position");
+        }, 300);
+      }, 3000);
+    }, 500);
+  }, []);
 
-            </section>
-        </main>
-    );
+  console.log(theme);
+  return (
+    <main
+      className={`home-main main-${theme} flex column align-end space-start`}
+    >
+      <section className="home-content flex column align-center space-center">
+        <div
+          className={`home-header flex column align-center space-center ${contentClass}`}
+        >
+          <p className={headerTitleClass}>Welcome to </p>
+          <p>GRAFIT.</p>
+        </div>
+        <p>
+          A design driven, go-to tool for researching and analyzing your
+          favorite Movies & TV Shows, all using cutting edge tech like React,
+          SASS, Redux alongside beautiful, self-built components!
+        </p>
+        <div className="home-images flex align-center space-between">
+          <IconTv className="type-icon" />
+          <IconFilm className="type-icon" />
+        </div>
+        <p>
+          Explore the bigger picture of any movie you can think of, with a rare
+          insight you've never seen before.
+        </p>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
